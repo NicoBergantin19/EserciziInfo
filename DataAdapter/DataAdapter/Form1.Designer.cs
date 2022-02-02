8d296c7f99731dce5cc69cd5507bb38511601b32
Bergantin Nico <75247663+NicoBergantin19@users.noreply.github.com>
2022-02-02 11:21:31 +0100

    DataAdapter SQL

---
+++ DataAdapter/DataAdapter/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace DataAdapter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(540, 198);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(595, 11);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(301, 199);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 213);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 19);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Visualizza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(493, 213);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(921, 11);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(301, 199);
+            this.listBox2.TabIndex = 5;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 249);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ DataAdapter/DataAdapter/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace DataAdapter
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        DataTable Dt;
+        SqlDataAdapter Da;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Da = new SqlDataAdapter();
+            Dt = new DataTable("Marche");
+            con = new SqlConnection(@"Data Source=INFORMATICA216\INFORMATICA225;Initial Catalog=master;Integrated Security=True");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            SqlConnection connDB = new SqlConnection($@"Data Source=INFORMATICA216\INFORMATICA225;Initial Catalog={listBox1.SelectedItem};Integrated Security=True");
+
+            connDB.Open();
+
+            /*string query = $"SELECT * FROM {listBox2.SelectedItem}";
+            Da.SelectCommand = con.CreateCommand();
+            Da.SelectCommand.CommandType = CommandType.Text;
+            Da.SelectCommand.CommandText = query;
+
+            Da.Fill(Dt);
+            dataGridView1.DataSource = Dt;*/
+
+            Dt = connDB.GetSchema("tables");
+            foreach (DataRow riga in Dt.Rows)
+            {
+                listBox2.Items.Add(riga["table_name"].ToString());
+            }
+
+            connDB.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*string query1 = "Select * FROM Marche";
+            //Da = new SqlDataAdapter(query1, con);
+            //Dt = new DataTable("Marche");
+            Da.SelectCommand = con.CreateCommand();
+            Da.SelectCommand.CommandType = CommandType.Text;
+            Da.SelectCommand.CommandText = query1;
+
+            con.Open();
+            Da.Fill(Dt);
+            dataGridView1.DataSource = Dt;
+            con.Close();*/
+
+            con.Open();
+            Dt = con.GetSchema("databases");
+            //dataGridView1.DataSource = Dt;
+            foreach (DataRow table in Dt.Rows)
+            {
+                //dataGridView1.DataSource = Dt;
+                listBox1.Items.Add(table[0].ToString());
+            }
+
+            con.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection connDB = new SqlConnection($@"Data Source=INFORMATICA216\INFORMATICA225;Initial Catalog={listBox1.SelectedItem};Integrated Security=True");
+            connDB.Open();
+
+            string query = $"SELECT * FROM {listBox2.SelectedItem}";
+            Da = new SqlDataAdapter(query, connDB);
+            Dt = new DataTable("Tabella");
+            Da.Fill(Dt);
+            dataGridView1.DataSource = Dt;
+
+            connDB.Close();
+        }
+    }
+}
