99f371c3a32e199f312e35f6f08f573e398d2e07
Bergantin Nico <75247663+NicoBergantin19@users.noreply.github.com>
2022-03-06 12:03:07 +0100

    Update ControlloHash.py

--- ControlloHash.py
+++ ControlloHash.py
@@ -1,33 +1,41 @@
 from genericpath import exists
 import hashlib
 import sys
-import os
 
-def controllo_file(filename):
+def Calcolo_Hash(filename):
+    file = open(filename, 'rb')
+    stringaSHA = hashlib.sha256()
+    dati = 0
+    while dati != b'':
+        dati = file.read(4096)
+        stringaSHA.update(dati)
+
+    stringaSHA = stringaSHA.hexdigest().upper()
+    return stringaSHA
+
+def Controllo_File(filename):
     file_exists = exists(filename)
     if(file_exists == False):
         print("Il file non esiste")
         exit()
 
-def hash_file(filename):
-    file = open(filename, 'rb')
-    stringaSHA = ''
-    while True:
-        dati = os.read(filename, 4096)
-        if (dati == False):
-            break
-        stringaSHA += hashlib.sha256(dati).hexdigest()
+if(len(sys.argv) != 3):
+    print(f'Usare {sys.argv[0]} file1 file2')
+    exit(1)
 
-    print(stringaSHA)
+filename = sys.argv[1]
+daControllare = sys.argv[2]
+Controllo_File(filename)
 
-def controllo_hash(originale, calcolato):
-    if(calcolato == originale):
-        print("L'hash in SHA256 è stato calcolato correttamente")
-    else:
-        print("Calcolo non corretto")
+originaleSHA = Calcolo_Hash(daControllare)
+stringaSHA = Calcolo_Hash(filename)
 
+print(f'SHA256 di {filename}: {stringaSHA}')
+print(f'SHA256 di {daControllare}: {originaleSHA}')
 
-originaleSHA = b'8FCAF8DF9E5DA18EEBD38B9EA4E47C0D51A64EFF2F9B600978430F535CFA0BFE' #inserire sha originale // calcolarlo
-controllo_file(sys.argv[1])
-calcolato = hash_file(sys.argv[1])
-controllo_hash(originaleSHA, calcolato)
+if(filename == daControllare):
+    if(originaleSHA == stringaSHA):
+        print("Hash SHA256 calcolato correttamente")
+else:
+    print("Sono stati inseriti dei file diversi")
+    
